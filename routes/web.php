00fc2aeefe5a3a6f8c197bcec5e2fa562239976d
ShahidulMamun <mdshahidulcse@gmail.com>
2026-09-14 19:04:19 +0600

    feat(courier): add courier routes to web

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\Admin\FaqManageController  as AdminFaqController;
 use App\Http\Controllers\Admin\SubscriberController;
 use App\Http\Controllers\Admin\ReviewManageController;
 use App\Http\Controllers\Admin\SettingsController;
+use App\Http\Controllers\Admin\CourierController;
 
 //== Guest Route ==
 Route::get('/', [LandingPageController::class, 'index'])->name('landing');
@@ -57,6 +58,7 @@ Route::middleware('admin.auth')->prefix('admin')->name('admin.')->group(function
     Route::resource('reviews', ReviewManageController::class)->only(['index', 'update', 'destroy']);
     Route::get('settings', [SettingsController::class, 'edit'])->name('settings.edit');
     Route::put('settings', [SettingsController::class, 'update'])->name('settings.update');
+    Route::resource('couriers', CourierController::class);
 });
 
 //== Logout Route == 
